37d5931b60bbce4f792fbffe298b72f944985f55
Md-Abraar <94089831+Md-Abraar@users.noreply.github.com>
2023-11-27 06:13:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,48 @@ binInp.addEventListener("input", () => {
     return true;
   }
 });
+
+octInp.addEventListener("input", () => {
+  let octValue = octInp.value;
+  if (octValidator(octValue)) {
+    let decValue = parseInt(octValue, 8);
+    decInp.value = octInp.value == "" ? "" : decValue;
+    binInp.value = octInp.value == "" ? "" : decValue.toString(2);
+    hexInp.value = octInp.value == "" ? "" : decValue.toString(16).toUpperCase();
+    errorMsg.textContent = "";
+  } else {
+    errorMsg.textContent = "Please Enter a Valid Octal Input";
+  }
+});
+
+hexInp.addEventListener("input", () => {
+  let hexValue = hexInp.value;
+  if (hexValidator(hexValue)) {
+    let decValue = parseInt(hexValue, 16);
+    decInp.value = hexInp.value == "" ? "" : decValue;
+    binInp.value = hexInp.value == "" ? "" : decValue.toString(2);
+    octInp.value = hexInp.value == "" ? "" : decValue.toString(8);
+    errorMsg.textContent = "";
+  } else {
+    errorMsg.textContent = "Please Enter a Valid Hexadecimal Input";
+  }
+});
+
+function octValidator(num) {
+  for (let i = 0; i < num.length; i++) {
+    if (!/^[0-7]$/.test(num[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function hexValidator(num) {
+  for (let i = 0; i < num.length; i++) {
+    if (!/^[0-9A-Fa-f]$/.test(num[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
